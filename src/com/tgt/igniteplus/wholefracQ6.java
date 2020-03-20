05c0a9ec7e07cd72e7e7ece3b679345945bc3910
pragathiindran <50520367+pragathiindran@users.noreply.github.com>
2020-03-20 14:51:31 +0530

    Six

--- src/com/tgt/igniteplus/wholefracQ6.java
+++ src/com/tgt/igniteplus/wholefracQ6.java
@@ -3,13 +3,19 @@ package com.tgt.igniteplus;
 import java.util.*;
 public class wholefracQ6 {
     public static void main(String[] args){
-        Scanner in=new Scanner(System.in);
-        double num,whole_part,fraction_part;
-        System.out.print("enter the decimal number");
-        num=in.nextDouble();
-        fraction_part=num%1;
-        whole_part=num-fraction_part;
-        System.out.println("integer part"+whole_part);
-        System.out.print("fraction part"+fraction_part);
+        double n, temp = 0.0, whole = 0, frac = 0;
+        int i = 1;
+        System.out.println("Enter the value of n:");
+        Scanner in = new Scanner(System.in);
+        n = in.nextDouble();
+        do {
+            i = i * 10;
+            temp = n * i;
+        } while ((temp % 10) != 0);
+        i = i / 10;
+        temp = (int) n;
+        frac = (n * i) - (temp * i);
+        System.out.println("Actual value: " + n);
+        System.out.println("Whole part: " + (int) n + "\nFraction part: " + (int) frac);
     }
 }
