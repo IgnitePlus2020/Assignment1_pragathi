d13b283af33d96d06d2b569e873774e21d861843
pragathiindran <50520367+pragathiindran@users.noreply.github.com>
2020-03-26 19:50:19 +0530

    Updated after review

--- src/com/tgt/igniteplus/sQ15.java
+++ src/com/tgt/igniteplus/sQ15.java
@@ -7,7 +7,7 @@ public class sQ15 {
         System.out.print("enter the value of n: ");
         Scanner in=new Scanner(System.in);
         n=in.nextInt();
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n+1; i++) {
             if (i==4)
                 continue;
             System.out.print(i * i + "  ");
