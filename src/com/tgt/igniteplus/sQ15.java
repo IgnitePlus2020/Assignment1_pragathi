3e8022c162ef90bf96baa9b155e0d54e1d206aec
pragathiindran <50520367+pragathiindran@users.noreply.github.com>
2020-03-26 19:43:13 +0530

    updated after review

--- src/com/tgt/igniteplus/sQ15.java
+++ src/com/tgt/igniteplus/sQ15.java
@@ -1,4 +1,4 @@
-//a program to print sequence 1,4,9,25,36,49.......N
+//a program to print sequence 1,4,9,25,36,49.......N(make sure that 16=4^2 is not present)
 package com.tgt.igniteplus;
 import java.util.Scanner;
 public class sQ15 {
@@ -8,7 +8,7 @@ public class sQ15 {
         Scanner in=new Scanner(System.in);
         n=in.nextInt();
         for (int i = 1; i <= n; i++) {
-            if (i % 4 == 0)
+            if (i==4)
                 continue;
             System.out.print(i * i + "  ");
         }
